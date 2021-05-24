dd9ada3b22a6b3a09abd9b771f2ce58cf421f630
AngelaSophia <ayugisophiaangela@gmail.com>
2021-05-24 21:48:34 +0300

    sad

--- data.js
+++ data.js
@@ -1,39 +1,39 @@
 
 
 
-// var sum =10+10;
-// console.log(sum);
-// var myVar=45;
-// myVar++;
-// console.log(myVar);
-// var remainder;
-// remainder=11%3;
-// console.log(remainder );
-// // escaping strings
-// var myStr="my name is\"\Ayugi\"\and am from Homa bay";
-// console.log(myStr);
-// //  concatinating strings
-// var myStr="my name is "  + "Ayugi Sophia.  ";
-// console.log(myStr);
-// // appending variables with string
-// var someAdjective="worthwhile";
-// var myStr="am learning code";
-// myStr += someAdjective;
-// console.log(myStr);
-// // getting length of a String
-// var firstName="Lisa lab";
-// var lastName="Lovelace";
-// console.log(firstName.length);
-// console.log(firstName[0]);
-// console.log(lastName.length-1);
-// console.log(lastName[5]);
+var sum =10+10;
+console.log(sum);
+var myVar=45;
+myVar++;
+console.log(myVar);
+var remainder;
+remainder=11%3;
+console.log(remainder );
+// escaping strings
+var myStr="my name is\"\Ayugi\"\and am from Homa bay";
+console.log(myStr);
+//  concatinating strings
+var myStr="my name is "  + "Ayugi Sophia.  ";
+console.log(myStr);
+// appending variables with string
+var someAdjective="worthwhile";
+var myStr="am learning code";
+myStr += someAdjective;
+console.log(myStr);
+// getting length of a String
+var firstName="Lisa lab";
+var lastName="Lovelace";
+console.log(firstName.length);
+console.log(firstName[0]);
+console.log(lastName.length-1);
+console.log(lastName[5]);
 
 
-// function words(myNoun,myAdjective,myVerb,myAdverb){
-//     var result="";
-//     return result;
-// }
-// console.log(words("cat","small","run","faster"));
+function words(myNoun,myAdjective,myVerb,myAdverb){
+    var result="";
+    return result;
+}
+console.log(words("cat","small","run","faster"));
 
 // NESTED FUNCTIONS
 var a=40;
