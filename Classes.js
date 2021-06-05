7a815d2e0bb5534485acf8b87f629d798ad0837b
AngelaSophia <ayugisophiaangela@gmail.com>
2021-06-05 09:36:01 +0300

    Happy

--- Classes.js
+++ Classes.js
@@ -1,5 +1,5 @@
-// Classes using functions
-// When creating a class using a function you start with this keyword and a new keyword
+//This is a constroctor function
+//When creating a constrocto using a function you start with this keyword and a new keyword
 
 function Mango(types){
     this.types=types;
@@ -18,7 +18,7 @@ console.log(numbers.length);
 console.log(numbers[0]);
 console.log(numbers[3]);
 
-console.log(numbers.slice());
+//console.log(numbers.slice());
 console.log(numbers);
 
 
@@ -39,6 +39,45 @@ console.log(cars);
 console.log(cars.shift());
 console.log(cars.pop());
 console.log(cars);
+
+function student(name){ //used to check if the paramater passed is an array or note.
+    return Array.isArray(name);
+    
+}
+console.log(student(["Ayugi","Sophia","Angela"]));
+
+const numbers=[2,5,8,9,7];
+const square=numbers.map(x => x*2);
+console.log(square);
+
+const digits=[2,4,6,7,9];
+const result=digits.map(n => n*n);  // MAP is used to call specific function in Element. 
+console.log(result);
+
+let sophia={name:'Ayugi Angela'},
+     laura={name:'Betty Laura'},
+     brawlian={name:'Annemie Brawlian'};
+let userRoles=new Map();
+console.log(typeof(userRoles));
+//console.log(userRoles instanceof Map);
+ console.log(userRoles.set(laura, 'kid'));  
+ 
+ //creating a new map object
+ let canditaes=new Map([
+     [sophia,'admin'],
+     [laura,'kid'],
+     [brawlian,'first']
+ ]);
+ console.log(canditaes.get(sophia));
+ console.log(canditaes.get(laura));
+ console.log(canditaes.get(brawlian));
+ console.log(canditaes.size);
+ console.log(typeof canditaes);
+ 
+
+
+
+
 
 
 
